f5fedbce7230bfe6c703d96ca3ae02dc478186ad
m-aggie <60620923+m-aggie@users.noreply.github.com>
2020-11-20 13:17:03 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -3,7 +3,14 @@ colour = input("What is your favourite colour? ")
 print(f"{colour}! that is my favourite colour too")
 
 #2
-cans=int(input("How many cans come in the pack? "))
-pack=int(input("How many packs are there? "))
+cans = int(input("How many cans come in the pack? "))
+pack = int(input("How many packs are there? "))
 total = cans*pack
 print(f"The total number of cans is {total}")
+
+#3
+height = float(input("Enter the height: "))
+width = float(input("Enter the width: "))
+length = float(input("Enter the length: "))
+volume = height*width*length
+print(f"The volume is {volume}m3")

4de1002a92218b4596bfbbe166a5da534e960d25
m-aggie <60620923+m-aggie@users.noreply.github.com>
2020-11-27 13:42:08 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,59 @@
+#1
+i = 271
+while i <= 319:
+ print(i)
+ i += 3 
+ 
+#2
+i = -5
+while i <= 85:
+  print(i)
+  i += 3
+  
+#3
+i = -82
+while i <=39:
+  print(i)
+  i += 11
+  
+#4
+i = 377
+while i <= 1074:
+  print(i)
+  i += 17
+  
+#5
+i = -180
+while i <= 63:
+  print(i)
+  i += 9
+  
+#6
+i = 45
+while i <= 45:
+  print(i)
+  i += 0
+  
+#7
+i = -4
+while i <= 386:
+  print(i)
+  i += 15
+  
+#8
+i = 482
+while i <= 706:
+  print(i)
+  i += 14
+  
+#9
+i = -241
+while i <= 239:
+  print(i)
+  i += 12
+  
+#10
+i = -216
+while i <= -76:
+  print(i)
+  i += 7
